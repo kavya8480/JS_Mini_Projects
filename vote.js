416492ac57aac266a1423ad585fe92f4291bfda6
kavya8480 <tapaswi84802@gmail.com>
2026-01-27 21:01:01 +0530

    read me

---
+++ vote.js
@@ -0,0 +1,32 @@
+function handleclick(){
+  let age = prompt("Enter your age", "age");
+  if(age>=18) {
+    alert("You are eligible for vote", age);
+   
+  }
+  else {
+   alert("You are not eligible for vote",age);
+  }
+
+  }
+  let stylechng= document.getElementById("divv")
+  stylechng.style.backgroundColor="pink"
+
+  let sec=document.getElementById("sec")
+ 
+setTimeout(print,1500)
+
+function print(){
+  console.log('hello world');
+}
+
+
+setInterval(print1,1500)
+
+function print1(){
+  console.log('hii, its me kavya.');
+}
+
+
+
+  
